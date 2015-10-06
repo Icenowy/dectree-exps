45924de3de3fa359c8aa6be310c76d780001439a
Icenowy Zheng <icenowy@outlook.com>
2015-10-07 00:32:44 +0800

    isutil: add test for util_datadivide

--- tests/isutil.cpp
+++ tests/isutil.cpp
@@ -26,5 +26,19 @@ int main()
 	dt.enums.push_back(1);
 	assert(util_datajudge(dv,dt));
 
+	dt.numbers.clear();
+	dt.enums.clear();
+
+	dt.enums.push_back(1);
+	std::vector<isdectree::Datum> dtv;
+	dtv.push_back(dt);
+	dt.enums.clear();
+	dt.enums.push_back(0);
+	dtv.push_back(dt);
+
+	std::pair<std::vector<isdectree::Datum>, std::vector<isdectree::Datum> > dtvpr = util_datadivide(dv,dtv);
+	assert(dtvpr.first.size() == 1);
+	assert(dtvpr.first.size() == 1);
+
 	return 0;
 }
