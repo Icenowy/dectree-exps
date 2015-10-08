5f28e0ca73410d536f42459df229941c98b9702a
Icenowy Zheng <icenowy@outlook.com>
2015-10-08 21:44:36 +0800

    done.

--- id3.cpp
+++ id3.cpp
@@ -30,6 +30,27 @@ namespace isdectree {
 		double entropy_new = (leftlen/len) * entropy_left + (1-leftlen/len) * entropy_right;
 		return original_entropy - entropy_new;
 	}
+	static std::vector<std::set<EnumType> > full_set(EnumType i)
+	{
+		if(i < 0) throw std::runtime_error("cannot get full_set of a minus number");
+		if(i == 0) {
+			std::vector<std::set<EnumType> > v;
+			std::set<EnumType> s;
+			s.insert(0);
+			v.push_back(s);
+			return v;
+		}
+		else {
+			std::vector<std::set<EnumType> > v;
+			std::vector<std::set<EnumType> > ov = full_set(i-1);
+			for(std::set<EnumType> os : ov) {
+				v.push_back(os);
+				os.insert(i);
+				v.push_back(os);
+			}
+			return v;
+		}
+	}
 	Node *algorithm_id3(const DatasetMetadata &meta, const std::vector<Datum> &data) {
 		/*Divide dv;
 		double number_maxgain = -INFINITY,number_threshold = 0.0;
@@ -46,9 +67,24 @@ namespace isdectree {
 			throw std::runtime_error("Null dataset generated");
 		double max_gain = -INFINITY;
 		int max_gain_data = -1;
+#ifdef SIMP
 		int max_gain_item = -1;
+#else
+		std::set<EnumType> max_gain_set;
+#endif
 		for(int i = 0;i<meta.enum_count;i++) {
 			dv.paramater = i;
+#ifndef SIMP
+			for(std::set<EnumType> s : full_set(meta.maxenum[i])) {
+				dv.judgment = s;
+				double gain;
+				if((gain = algorithm_id3_gain(dv,meta,data)) > max_gain) {
+					max_gain = gain;
+					max_gain_data = i;
+					max_gain_set = s;
+				}
+			}
+#else
 			for(int j = 0;j<meta.maxenum[i];j++) {
 				dv.judgment.clear();
 				dv.judgment.insert(j);
@@ -59,10 +95,15 @@ namespace isdectree {
 					max_gain_item = j;
 				}
 			}
+#endif
 		}
 		dv.paramater = max_gain_data;
+#ifdef SIMP
 		dv.judgment.clear();
 		dv.judgment.insert(max_gain_item);
+#else
+		dv.judgment = max_gain_set;
+#endif
 		Node *node = new Node;
 		node->statement = dv;
 		int result = -1;
---
+++ judge-code/s-j.cpp
@@ -0,0 +1,79 @@
+#include <isdectree.h>
+#include <id3.h>
+#include <isutil.h>
+#include <isdecide.h>
+
+#include <vector>
+#include <sstream>
+#include <string>
+
+using namespace std;
+using namespace isdectree;
+
+#define BASENAME "bigandbig.csv"
+#define DATA_COL 6 //exclude the result
+#define DATA_LINE 12000
+#define JUDGE_BASENAME "waimai.csv"
+#define JUDGE_LINE 150
+
+int main()
+{
+	ostringstream oss;	
+	FILE **fps = new FILE*[DATA_COL+1];
+	for (int i = 0;i<DATA_COL+1;i++) {
+		oss.str("");
+		oss << BASENAME << "-nohead-" << i+2 << "-enumized";
+		fps[i] = fopen(oss.str().c_str(),"r");
+	}
+	DatasetMetadata meta;
+	for (int i = 0;i<DATA_COL;i++)
+		meta.maxenum.push_back(0); // init maxenum
+	meta.maxresult = 0;
+	meta.number_count = 0;
+	meta.enum_count = DATA_COL;
+	vector<Datum> data;
+	for (int l = 0;l<DATA_LINE;l++) {
+		Datum dt;
+		for(int i = 0;i<DATA_COL;i++) {
+			EnumType enumval;
+			fscanf(fps[i],"%u",&enumval);
+			dt.enums.push_back(enumval);
+			if(meta.maxenum[i] < enumval)
+				meta.maxenum[i] = enumval;
+		}
+		EnumType resval;
+		fscanf(fps[DATA_COL],"%u",&resval);
+		dt.result = resval;
+		if(meta.maxresult < resval)
+			meta.maxresult = resval;
+		data.push_back(dt);
+	}
+	for (int i = 0;i<DATA_COL+1;i++) {
+		fclose(fps[i]);
+	}
+	Node *node = algorithm_id3(meta,data);
+	data.clear();
+	for (int i = 0;i<DATA_COL;i++) {
+		oss.str("");
+		oss << JUDGE_BASENAME << "-nohead-" << i+2 << "-enumized";
+		fps[i] = fopen(oss.str().c_str(),"r");
+	}
+	for (int l = 0;l<JUDGE_LINE;l++) {
+		Datum dt;
+		for(int i = 0;i<DATA_COL;i++) {
+			EnumType enumval;
+			fscanf(fps[i],"%u",&enumval);
+			dt.enums.push_back(enumval);
+			if(meta.maxenum[i] < enumval)
+				meta.maxenum[i] = enumval;
+		}
+		data.push_back(dt);
+	}
+	for(Datum dt : data) {
+		if(decide(dt,node)-1)
+			printf("False\n");
+		else
+			printf("True\n"); //FIXME: HARD CODE
+	}
+	return 0;
+}
